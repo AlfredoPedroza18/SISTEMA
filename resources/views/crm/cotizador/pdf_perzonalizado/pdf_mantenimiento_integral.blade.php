23dd4830652dd0cd67f9539e524dd513c25655b0
AlfredoPedroza18 <l191080345@iztapalpa.tecnm.mx>
2024-03-13 10:47:16 -0600

    Cotizacion_Dsaix

--- resources/views/crm/cotizador/pdf_perzonalizado/pdf_mantenimiento_integral.blade.php
+++ resources/views/crm/cotizador/pdf_perzonalizado/pdf_mantenimiento_integral.blade.php
@@ -1,6 +1,14 @@
+@include("crm.cotizador.pdf_perzonalizado.plantillas.encabezado")
+
 <div style="page-break-after:always;">
-    <img id="logo" src="images/logo.png" alt="">
-    @include("crm.cotizador.pdf_perzonalizado.plantillas.tabla_productos");
-</div>
     
-<div style="page-break-after:always;">x</div>
+        <p>{{ isset($cotizacion) ? $cotizacion->fecha_cotizacion->format('d-m-Y') : \Carbon\Carbon::now()->format('d/m/Y') }}</p>
+        <p>AT’N.:</p>
+        <p>@if(isset($cotizacion)) {{ $cotizacion->cliente->nombre_comercial }} @else &nbsp; @endif</p>
+        <p>ASUNTO: </p>
+        <p class="sangria">POR MEDIO DEL PRESENTE TENGO EL AGRADO DE ENVIARLE LA SIGUIENTE COTIZACIÓN: SERVICIO DE SOFTWARE ESPECIALIZADO ERP NOIL PARA MÓDULO DE ALMACÉN Y COMPRAS; IMPLEMENTACIÓN, CAPACITACIÓN, DESARROLLO PERSONALIZADO, CONSULTORÍAS Y SOPORTE TÉCNICO.  </p>
+        <p class="sangria">TOMAMOS MUY EN CUENTA LA IMPORTANCIA DE NUESTROS PRODUCTOS Y SERVICIOS, ASÍ COMO EL COSTO-BENEFICIO QUE REPRESENTA PARA SU EMPRESA, POR LO CUAL DE ANTE MANO OFRECEMOS UN COMPROMISO TOTAL EN EL DESEMPEÑO DE NUESTROS TRABAJOS, ENFOCANDO LOS RECURSOS NECESARIOS PARA LOGRAR EL ÉXITO DEL PROYECTO.</p>
+        <p>SERVICIO MENSUAL</p>
+ 
+       <p>DETALLE DE COTIZACIÓN SOFTWARE NOIL </p>
+</div>
--- resources/views/crm/cotizador/pdf_perzonalizado/pdf_punto_venta.blade.php
+++ resources/views/crm/cotizador/pdf_perzonalizado/pdf_punto_venta.blade.php
@@ -1,6 +1,21 @@
+@include("crm.cotizador.pdf_perzonalizado.plantillas.encabezado")
+
 <div style="page-break-after:always;">
-    <img id="logo" src="" alt="">
-    @include("crm.cotizador.pdf_perzonalizado.plantillas.tabla_productos");
-</div>
     
-<div style="page-break-after:always;">x</div>
+        <p>{{ isset($cotizacion) ? $cotizacion->fecha_cotizacion->format('d-m-Y') : \Carbon\Carbon::now()->format('d/m/Y') }}</p>
+        <p>AT’N.:</p>
+        <p>@if(isset($cotizacion)) {{ $cotizacion->cliente->nombre_comercial }} @else &nbsp; @endif</p>
+        <p>POR MEDIO DEL PRESENTE TENGO EL AGRADO DE ENVIARLE LA SIGUIENTE COTIZACIÓN: </p>
+        <ul>
+            <li>PUNTO DE VENTA SOFTWARE ERP NOIL</li>
+            <li>ACCESORIOS</li>
+        </ul>
+        
+        <p>DETALLE DE COTIZACIÓN</p>
+
+
+        
+        @include("crm.cotizador.pdf_perzonalizado.plantillas.tabla_productos");
+</div>
+
+
